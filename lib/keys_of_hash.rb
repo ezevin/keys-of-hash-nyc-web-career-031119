fd2cb419d1a76686816decdaa70f6ef44095a641
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-19 18:48:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,8 @@ class Hash
   def keys_of(*arguments)
     # code goes here
 self.map do |key, value|
-  if arguments.compact.include?(value)
-  key
+  if arguments.include?(value)
+  key.map.compact
 end
 end 
  end
